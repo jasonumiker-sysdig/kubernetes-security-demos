6e1b05565493745965363c1c2b36e1304a103a6b
Jason Umiker <jason.umiker@sysdig.com>
2023-04-08 12:43:49 +1000

    Combined commands

--- setup-cluster/setup-microk8s-vm.sh
+++ setup-cluster/setup-microk8s-vm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # You can reset things back to defaults by deleting the VM and then let the script recreate
-multipass delete microk8s-vm
-multipass purge
+multipass delete microk8s-vm --purge
 
 # Provision your local cluster VM
 multipass launch --cpus 2 --memory 4G --disk 10G --name microk8s-vm --cloud-init cloud-init.yaml 22.04
