80a95f9966547c4750ad21d9a737950223ab8e63
Jason Umiker <jason.umiker@sysdig.com>
2023-04-08 12:28:07 +1000

    Added a Terraform option for microk8s

--- setup-cluster/bootstrap-microk8s-vm.sh
+++
@@ -1,4 +0,0 @@
-cd /home/ubuntu/
-git clone https://github.com/jasonumiker-sysdig/kubernetes-security-demos.git
-cd /home/ubuntu/kubernetes-security-demos/setup-cluster/
-sudo /home/ubuntu/kubernetes-security-demos/setup-cluster/setup-microk8s.sh
--- setup-cluster/setup-microk8s-vm.sh
+++ setup-cluster/setup-microk8s-vm.sh
@@ -5,20 +5,7 @@ multipass delete microk8s-vm
 multipass purge
 
 # Provision your local cluster VM
-multipass launch --cpus 2 --memory 4G --disk 20G --name microk8s-vm 22.04
-
-# Deploy and run setup-microk8s.sh to our new VM
-multipass transfer ./bootstrap-microk8s-vm.sh microk8s-vm:/home/ubuntu/
-if [[ $OS == "Windows_NT" ]]; then
-    multipass exec microk8s-vm -- sudo apt update -y
-    multipass exec microk8s-vm -- sudo apt install dos2unix -y
-    multipass exec microk8s-vm -- dos2unix //home/ubuntu/bootstrap-microk8s-vm.sh
-    multipass exec microk8s-vm -- chmod +x //home/ubuntu/bootstrap-microk8s-vm.sh
-    multipass exec microk8s-vm -- //home/ubuntu/bootstrap-microk8s-vm.sh
-else
-    multipass exec microk8s-vm -- chmod +x /home/ubuntu/bootstrap-microk8s-vm.sh
-    multipass exec microk8s-vm -- /home/ubuntu/bootstrap-microk8s-vm.sh
-fi
+multipass launch --cpus 2 --memory 4G --disk 10G --name microk8s-vm --cloud-init cloud-init.yaml 22.04
 
 # Copy the .kube/config to the local machine
 cd ~/.kube
