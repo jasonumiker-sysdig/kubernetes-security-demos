fb69be77ab0cf84755770a5342442d45f99c363e
Jason Umiker <jason.umiker@sysdig.com>
2023-04-12 14:15:44 +1000

    Updated security-playground

--- demos/security-playground/Dockerfile
+++ demos/security-playground/Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7-bullseye
+FROM python:3.11.3-bullseye
 
 RUN pip install pipenv
 
--- demos/security-playground/build.sh
+++ demos/security-playground/build.sh
@@ -1,6 +1,10 @@
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
+  --tag jasonumiker/security-playground:120423 \
+  .
+docker buildx build --push \
+  --platform linux/arm64,linux/amd64 \
   --tag jasonumiker/security-playground:latest \
   .
 docker buildx rm mybuilder
