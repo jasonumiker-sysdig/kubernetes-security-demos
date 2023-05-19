d7ab9a3825e16e4ebc8f7a25fea55b818d1bb1f3
Jason Umiker <jason.umiker@sysdig.com>
2023-05-19 14:29:03 +1000

    Don't overwrite the local kubeconfig by default

--- setup-cluster/setup-microk8s-vm.sh
+++ setup-cluster/setup-microk8s-vm.sh
@@ -7,5 +7,5 @@ multipass delete microk8s-vm --purge
 multipass launch --cpus 2 --memory 4G --disk 10G --name microk8s-vm --cloud-init cloud-init.yaml 22.04
 
 # Copy the .kube/config to the local machine
-cd ~/.kube
-multipass transfer microk8s-vm:/home/ubuntu/.kube/config config
+#cd ~/.kube
+#multipass transfer microk8s-vm:/home/ubuntu/.kube/config config
