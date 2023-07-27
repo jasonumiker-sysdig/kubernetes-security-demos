0efdc0b8e1120f4bc8b09a086389201cb8cbd869
Jason Umiker <jason.umiker@sysdig.com>
2023-07-27 16:34:57 +1000

    Updated to latest images

--- demos/security-playground/docker-build-security-playground/build-unprivileged.sh
+++ demos/security-playground/docker-build-security-playground/build-unprivileged.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker buildx create --name mybuilder --bootstrap --use --file ./Dockerfile-unprivileged
-docker buildx build --push \
+docker buildx create --name mybuilder --bootstrap --use
+docker buildx build --push --file ./Dockerfile-unprivileged \
   --platform linux/arm64,linux/amd64 \
   --tag public.ecr.aws/m9h2b5e7/security-playground-unprivileged:270723 \
   .
-docker buildx build --push \
+docker buildx build --push --file ./Dockerfile-unprivileged \
   --platform linux/arm64,linux/amd64 \
   --tag public.ecr.aws/m9h2b5e7/security-playground-unprivileged:latest \
   .
