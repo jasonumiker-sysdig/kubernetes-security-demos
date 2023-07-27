64fb1e57f8496db54363336f9f835f8117c95230
Jason Umiker <jason.umiker@sysdig.com>
2023-07-27 17:06:02 +1000

    Updating image

--- demos/network-policy/hello-app/build.sh
+++ demos/network-policy/hello-app/build.sh
@@ -1,10 +1,10 @@
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag jasonumiker/hello-app:180623 \
+  --tag public.ecr.aws/m9h2b5e7/hello-app:270723 \
   .
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag hello-app:latest public.ecr.aws/m9h2b5e7/hello-app:latest \
+  --tag public.ecr.aws/m9h2b5e7/hello-app:latest \
   .
 docker buildx rm mybuilder
